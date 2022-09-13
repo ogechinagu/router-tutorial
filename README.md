2ce5b191e319e1e664f0cb6244b1bd18f9892a89
ogechinagu <54102855+ogechinagu@users.noreply.github.com>
2022-09-13 09:44:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-b77wqa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-b77wqa)
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './main.css';
+
+let rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  rootElement
+);
